1c780f9ce55b12c68e6769ed6ad8821bdb4dc5c9
sunakoycu <sunakoycu@gmail.com>
2016-03-11 21:51:21 +0800

    first commit

---
+++ FinalProject/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.tbBinary = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tbDecimal = New System.Windows.Forms.TextBox()
+        Me.bConvert = New System.Windows.Forms.Button()
+        Me.bClear = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbHexa = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tbOctal = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tbBinary
+        '
+        Me.tbBinary.Enabled = False
+        Me.tbBinary.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbBinary.Location = New System.Drawing.Point(29, 39)
+        Me.tbBinary.Name = "tbBinary"
+        Me.tbBinary.Size = New System.Drawing.Size(100, 23)
+        Me.tbBinary.TabIndex = 0
+        Me.tbBinary.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(54, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Binary"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(47, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 19)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Decimal"
+        '
+        'tbDecimal
+        '
+        Me.tbDecimal.Font = New System.Drawing.Font("Calibri", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbDecimal.Location = New System.Drawing.Point(28, 75)
+        Me.tbDecimal.Name = "tbDecimal"
+        Me.tbDecimal.Size = New System.Drawing.Size(100, 40)
+        Me.tbDecimal.TabIndex = 2
+        Me.tbDecimal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'bConvert
+        '
+        Me.bConvert.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.bConvert.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bConvert.Location = New System.Drawing.Point(104, 214)
+        Me.bConvert.Name = "bConvert"
+        Me.bConvert.Size = New System.Drawing.Size(85, 27)
+        Me.bConvert.TabIndex = 4
+        Me.bConvert.Text = "Convert"
+        Me.bConvert.UseVisualStyleBackColor = True
+        '
+        'bClear
+        '
+        Me.bClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.bClear.Font = New System.Drawing.Font("Calibri", 10.0!)
+        Me.bClear.Location = New System.Drawing.Point(195, 214)
+        Me.bClear.Name = "bClear"
+        Me.bClear.Size = New System.Drawing.Size(85, 27)
+        Me.bClear.TabIndex = 5
+        Me.bClear.Text = "Clear"
+        Me.bClear.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(33, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 19)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Hexadecimal"
+        '
+        'tbHexa
+        '
+        Me.tbHexa.Enabled = False
+        Me.tbHexa.Font = New System.Drawing.Font("Calibri", 10.0!)
+        Me.tbHexa.Location = New System.Drawing.Point(30, 86)
+        Me.tbHexa.Name = "tbHexa"
+        Me.tbHexa.Size = New System.Drawing.Size(100, 24)
+        Me.tbHexa.TabIndex = 6
+        Me.tbHexa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(58, 110)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 19)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Octal"
+        '
+        'tbOctal
+        '
+        Me.tbOctal.Enabled = False
+        Me.tbOctal.Font = New System.Drawing.Font("Calibri", 10.0!)
+        Me.tbOctal.Location = New System.Drawing.Point(30, 132)
+        Me.tbOctal.Name = "tbOctal"
+        Me.tbOctal.Size = New System.Drawing.Size(100, 24)
+        Me.tbOctal.TabIndex = 8
+        Me.tbOctal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(171, 107)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 19)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "--to--"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.tbBinary)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.tbOctal)
+        Me.GroupBox1.Controls.Add(Me.tbHexa)
+        Me.GroupBox1.Location = New System.Drawing.Point(214, 30)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(158, 163)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.tbDecimal)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 30)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(158, 163)
+        Me.GroupBox2.TabIndex = 12
+        Me.GroupBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(384, 261)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.bClear)
+        Me.Controls.Add(Me.bConvert)
+        Me.Font = New System.Drawing.Font("Calibri", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Number System Converter"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents tbBinary As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tbDecimal As System.Windows.Forms.TextBox
+    Friend WithEvents bConvert As System.Windows.Forms.Button
+    Friend WithEvents bClear As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents tbHexa As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents tbOctal As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ FinalProject/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Dim decNumber As Integer
+    Dim decCheck As Boolean
+    Private Sub bConvert_Click(sender As Object, e As EventArgs) Handles bConvert.Click
+        If tbDecimal.Text <> "" Then
+            validateNum()
+            If decCheck = True Then
+                decNumber = CInt(tbDecimal.Text)
+                toBinary()
+                toHexa()
+                toOctal()
+            Else
+                MessageBox.Show("Invalid Input")
+            End If
+            
+        End If
+    End Sub
+
+    Private Sub bClear_Click(sender As Object, e As EventArgs) Handles bClear.Click
+        clear()
+    End Sub
+
+    Private Sub clear()
+        tbDecimal.Text = ""
+        tbBinary.Text = ""
+        tbHexa.Text = ""
+        tbOctal.Text = ""
+    End Sub
+
+    Private Sub validateNum()
+        Dim decSize As Integer
+        Dim decChar As String
+        Dim notDec As Integer
+        decSize = tbDecimal.Text.Length - 1
+        notDec = 0
+
+        For count As Integer = 0 To decSize
+            decChar = tbDecimal.Text.Substring(count, 1)
+            If Not IsNumeric(decChar) Then
+                notDec += 1
+            End If
+        Next
+
+        If notDec > 0 Then
+            decCheck = False
+        Else
+            decCheck = True
+        End If
+
+    End Sub
+
+    Private Sub toBinary()
+        Dim binString As String
+        Dim binStringFinal As String
+        Dim dividend As Integer
+        Dim quotient As Integer
+        Dim remainder As Integer
+        Dim quotientCheck As Boolean = True
+        dividend = CInt(tbDecimal.Text)
+
+        Do While quotientCheck = True
+            quotient = Math.Floor(dividend / 2)
+            remainder = dividend Mod 2
+            dividend = quotient
+            binString = binString & CStr(remainder)
+            If quotient = 0 Then
+                quotientCheck = False
+            End If
+        Loop
+
+        For count As Integer = binString.Length - 1 To 0 Step -1
+            binStringFinal = binStringFinal & binString.Substring(count, 1)
+        Next
+        tbBinary.Text = binStringFinal
+    End Sub
+
+    Private Sub toHexa()
+        tbHexa.Text = Hex(decNumber)
+    End Sub
+    Private Sub toOctal()
+        Dim octString As String
+        Dim octStringFinal As String
+        Dim dividend As Integer
+        Dim quotient As Integer
+        Dim remainder As Integer
+        Dim quotientCheck As Boolean = True
+        dividend = CInt(tbDecimal.Text)
+
+        Do While quotientCheck = True
+            quotient = Math.Floor(dividend / 8)
+            remainder = dividend Mod 8
+            dividend = quotient
+            octString = octString & CStr(remainder)
+            If quotient = 0 Then
+                quotientCheck = False
+            End If
+        Loop
+
+        For count As Integer = octString.Length - 1 To 0 Step -1
+            octStringFinal = octStringFinal & octString.Substring(count, 1)
+        Next
+        tbOctal.Text = octStringFinal
+    End Sub
+End Class
